7a80f4f419782bc3a80f54b5b5ea7a500b0d2f71
Ville Peurala <ville.peurala@gmail.com>
2017-11-18 16:18:00 +0200

    Close connection.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const util = require("util");
 amqp.connect("amqp://localhost").then(function(connection) {
     console.log("util.inspect(connection): " + util.inspect(connection));
     console.log("Object.getOwnPropertyNames(connection): " + Object.getOwnPropertyNames(connection));
-
+    connection.close()
 }, function(error) {
     console.log("error: " + util.inspect(error));
 });
