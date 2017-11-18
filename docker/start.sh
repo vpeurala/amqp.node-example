61d2ad14d25a7babc6435fe238ec98c2a69b4e98
Ville Peurala <ville.peurala@gmail.com>
2017-11-18 17:17:07 +0200

    Producer and consumer talk to each other!

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex;
 PROJECT_ROOT=$(git rev-parse --show-toplevel);
-docker container create --hostname amqp-example --name amqp-example vpeurala/amqp-example:latest
+docker container rm -f amqp-example || true;
+docker container create --hostname amqp-example --name amqp-example --publish 4369:4369 --publish 5671:5671 --publish 5672:5672 --publish 15671:15671 --publish 15672:15672 --publish 25672:25672 vpeurala/amqp-example:latest
 docker container start amqp-example
