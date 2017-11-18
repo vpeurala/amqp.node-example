ce9fbec78c7d428ae1a2dfdcdffd2a644a324819
Ville Peurala <ville.peurala@gmail.com>
2017-11-18 16:16:09 +0200

    Try an explicit URL.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const amqp = require("amqplib");
 const util = require("util");
 
-amqp.connect().then(function(connection) {
+amqp.connect("amqp://localhost").then(function(connection) {
     console.log("util.inspect(connection): " + util.inspect(connection));
     console.log("Object.getOwnPropertyNames(connection): " + Object.getOwnPropertyNames(connection));
 
