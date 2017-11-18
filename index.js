f45698bc005c0a9f5f9cda93ef168618853ab8c3
Ville Peurala <ville.peurala@gmail.com>
2017-11-18 15:19:06 +0200

    Get connection and debug print it.

---
+++ index.js
@@ -0,0 +1,10 @@
+const amqp = require("amqplib");
+const util = require("util");
+
+amqp.connect().then(function(connection) {
+    console.log("util.inspect(connection): " + util.inspect(connection));
+    console.log("Object.getOwnPropertyNames(connection): " + Object.getOwnPropertyNames(connection));
+
+}, function(error) {
+    console.log("error: " + util.inspect(error));
+});
