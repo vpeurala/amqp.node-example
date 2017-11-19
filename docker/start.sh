ea8d1f0baa65c1270e37570f445b0b4925361fa8
Ville Peurala <ville.peurala@gmail.com>
2017-11-19 02:55:17 +0200

    Check with docker inspect and jq whether the container is already running.

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -ex;
 PROJECT_ROOT=$(git rev-parse --show-toplevel);
-docker container rm -f amqp-example || true;
+
+CONTAINER_STATUS=$(docker inspect amqp-example | jq '.[0].State.Status');
+
+if [ ${CONTAINER_STATUS} = '"running"' ]
+then
+  echo "Running.";
+else
+  echo "Not running.";
+fi
+
+# docker container rm -f amqp-example || true;
 docker container create --hostname amqp-example --name amqp-example --publish 4369:4369 --publish 5671:5671 --publish 5672:5672 --publish 15671:15671 --publish 15672:15672 --publish 25672:25672 vpeurala/amqp-example:latest
 docker container start amqp-example
